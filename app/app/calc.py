d99e20bf0194857bd664159a6cef5615b400ec4a
Ziyodullokhon <studiovisual0077@gmail.com>
2022-08-05 18:20:36 -0400

    Cofigured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return results"""
+    return x+y
+
+
+def subtract(x, y):
+    """Subtraact x from y and return result."""
+    return y-x
