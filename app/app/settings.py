40889802225daf84b5a695f52dee6c648cc87689
Ziyodullokhon <studiovisual0077@gmail.com>
2022-08-07 17:49:05 -0400

    Build user API (1)

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,9 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
 
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
